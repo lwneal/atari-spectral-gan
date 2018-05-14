95d5da2baa14d4f2b72bd2d41e0c0d019bc1f995
Lawrence Neal <nealla@lwneal.com>
2018-05-14 10:17:36 -0700

    Simplify sample.py

--- sample.py
+++ sample.py
@@ -17,40 +17,35 @@ parser.add_argument('--checkpoint_dir', type=str, default='checkpoints')
 parser.add_argument('--image_dir', type=str, default='input/')
 parser.add_argument('--video_name', type=str, default='sample')
 parser.add_argument('--latent_dim', type=int, default=4)
+parser.add_argument('--generator_filename', type=str, required=True)
 
 args = parser.parse_args()
 
-GENERATOR_FILENAME = 'checkpoints/gen_34'
 
-print('building model...')
-Z_dim = args.latent_dim
 
-generator = model.Generator(Z_dim).cuda()
+def main():
+    print('building model...')
+    Z_dim = args.latent_dim
 
-print('Loading model...')
-generator.load_state_dict(torch.load(GENERATOR_FILENAME))
-print('Loaded model')
+    generator = model.Generator(Z_dim).cuda()
 
-output_video_name = args.video_name
-output_count = 1
-fixed_z = Variable(torch.randn(args.batch_size, Z_dim).cuda())
-fixed_zprime = Variable(torch.randn(args.batch_size, Z_dim).cuda())
-def evaluate():
+    print('Loading model...')
+    generator.load_state_dict(torch.load(args.generator_filename))
+    print('Loaded model')
+
+    output_video_name = args.video_name
+    output_count = 1
+    fixed_z = Variable(torch.randn(args.batch_size, Z_dim).cuda())
+    fixed_zprime = Variable(torch.randn(args.batch_size, Z_dim).cuda())
+
     v = imutil.VideoMaker(output_video_name)
     for i in range(400):
         theta = abs(i - 200) / 200.
         z = theta * fixed_z + (1 - theta) * fixed_zprime
-        print(z.cpu().data)
         samples = generator(z[output_count]).cpu().data.numpy()
         samples = samples.transpose((0,2,3,1))
         v.write_frame(samples)
     v.finish()
-
-
-def main():
-    print('creating checkpoint directory')
-    os.makedirs(args.checkpoint_dir, exist_ok=True)
-    evaluate()
 
 
 if __name__ == '__main__':
